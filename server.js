c2ec4c591de28f649f120b06a003e67a34492a02
Mel Maki <melody.rackham@gmail.com>
2019-12-04 11:12:44 -0700

    Wasn't sure where to even start

--- server.js
+++ server.js
@@ -4,17 +4,25 @@ const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
+// CUSTOM MIDDLEWARE!
+//write a gatekeeper middleware that reads a password from the headers and if the password is "mellon", let it continue
+// if not, send back status code 401 and a message. Use it for the /area51 endpoint
+
 server.use(express.json());
 
 server.use('/api/hubs', hubsRouter);
 
 server.get('/', (req, res) => {
-  const nameInsert = (req.name) ? ` ${req.name}` : '';
+  const nameInsert = req.name ? ` ${req.name}` : '';
 
   res.send(`
     <h2>Lambda Hubs API</h2>
     <p>Welcome${nameInsert} to the Lambda Hubs API</p>
     `);
+});
+
+server.get('./area51', helmet(), (req, res) => {
+  res.send(req.headers);
 });
 
 module.exports = server;
