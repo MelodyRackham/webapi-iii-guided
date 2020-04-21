384981b1f5afd6989167314518ab022c71357aa2
Mel Maki <melody.rackham@gmail.com>
2020-04-21 10:51:46 -0600

    Adds helmet third party middleware and notes.md file

---
+++ notes.md
@@ -0,0 +1,7 @@
+MIDDLEWARE:
+
+Three types:
+
+1. Built in (to the Express object) .. no need to bring in any third party libraries
+2. Third party - similiar to built in, something that someone else has made for us to add more functionality to our app. Example = helmet. Helmet helps you secure your express apps by setting various HTTP headers.
+3. Custom -
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express'); // importing a CommonJS module
-
+const helmet = require('helmet');
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
@@ -8,8 +8,21 @@ const server = express();
 //write a gatekeeper middleware that reads a password from the headers and if the password is "mellon", let it continue
 // if not, send back status code 401 and a message. Use it for the /area51 endpoint
 
-server.use(express.json());
+function gateKeeper(req, res, next) {
+  const password = req.headers.password;
+  if (password && password.toLowerCase() === 'mellon') {
+    next();
+  } else {
+    res.status(401).json({ you: 'shall not pass!!' });
+  }
+}
+// built in middleware
+server.use(express.json()); // this is middleware and it parses the body for us!
 
+// third party middleware
+server.use(helmet());
+
+// router
 server.use('/api/hubs', hubsRouter);
 
 server.get('/', (req, res) => {
